d8ac0a1fa225e0c3d44c73c67ba3f067c04f5ad2
Sami Farhat <samikfarhat@gmail.com>
2019-03-30 11:11:42 +0000

    another lazy alias in source.sh

--- source.sh
+++ source.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+alias s='source source.sh'
 alias c='ghci coinproblem.lhs test.hs'
 alias o='open report.pdf'
 alias r='pandoc -f markdown+lhs coinproblem.lhs -V papersize:a4 -V fontsize:12pt -V geometry:margin=1in --pdf-engine=xelatex -o report.pdf; o'
