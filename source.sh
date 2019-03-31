28e118403802be0479c157f2b89a8663518021a0
Sami Farhat <samikfarhat@gmail.com>
2019-03-31 15:46:40 +0100

    add linting alias to source.sh

--- source.sh
+++ source.sh
@@ -3,4 +3,5 @@
 alias s='source source.sh'
 alias c='ghci coinproblem.lhs test.hs'
 alias o='open report.pdf'
+alias l='~/Library/Haskell/bin/hlint coinproblem.lhs'
 alias r='pandoc -f markdown+lhs coinproblem.lhs -V papersize:a4 -V fontsize:12pt -V geometry:margin=1in --pdf-engine=xelatex -o report.pdf; o'
